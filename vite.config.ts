64f78731c5a61e359f0bf64b6c5c74fe98eb375c
Martin Lui <robostrike.me@gmail.com>
2025-04-01 18:36:27 +0900

    configure changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/MuuuuuuM/",
@@ -10,8 +11,5 @@ export default defineConfig({
   },
   preview: {
     port: 4000
-  },
-  define: {
-    'process.env': {}, // Prevent "process is not defined" errors
-  },
-});
+  }
+})
